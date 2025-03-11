3526d3344eb8e12f2eebd5a0afecc91defcb43e2
Sathvika B <serein.reverie.meraki@gmail.com>
2025-03-11 11:42:12 +0530

    Implementing insert an entity in react app assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 // src/App.jsx
 
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import AddBook from './components/AddBook';
 import './App.css'; // Import global styles
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home/   >}></Route>
+        <Route path='/add-book' element={<AddBook/>}></Route>
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
